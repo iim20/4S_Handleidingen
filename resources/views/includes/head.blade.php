c43806cdd2a357902ba826b36c1cf0d4aa8b8076
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-09-23 11:09:27 +0200

    Link voor font-awesome

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,5 +7,7 @@
 <meta name="google-site-verification" content="Fdb3h6i9VSbRysXMwdteA4E2tZnedlygCcIyJBADqKo" />
 
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
 
 <title>Download your manual: Free user guides for all brands and devices!</title>
