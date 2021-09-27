78950f81f51140db4dcd8fb98ec25002d74aed9f
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-09-27 12:32:44 +0200

    #25 Nieuwe blade voor test data

---
+++ resources/views/pages/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket 1Extra</title>
+</head>
+<body style="text-align: center;">
+    <h1>Hello world!</h1>
+    <p>Ilyas Mohamed</p>
+    <p style="margin-top: 40%;">&copy; Ticket 1Extra</p>
+</body>
+</html>
